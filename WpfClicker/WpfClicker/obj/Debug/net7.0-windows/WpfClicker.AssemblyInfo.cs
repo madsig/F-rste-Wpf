c5571e82fcf532f7f0ecae912252fa199bb4069a
madsig <madsig@getacademy.no>
2023-11-24 14:29:09 +0100

    x

--- WpfClicker/WpfClicker/obj/Debug/net7.0-windows/WpfClicker.AssemblyInfo.cs
+++ WpfClicker/WpfClicker/obj/Debug/net7.0-windows/WpfClicker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfClicker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92618783f1be5d711f56edb4c58c066b295ff5d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfClicker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfClicker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
